75e8269f042186157a6770c51d8ed77ecc465109
abimakesthings <abitriesthings@gmail.com>
2025-07-26 21:59:29 -0700

    file restructuring

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-import RPi.GPIO as GPIO
+from gpiozero import Button
+from time import sleep
+from datetime import datetime
 
-BUTTON_PIN = 16
+button = Button(16)
+
+has_printed = False
+
+while True:
+    if button.is_pressed and not has_printed: #to trigger only once per press
+        print("Button was pressed")
+        has_printed = True
+        sleep(0.2) #delay to avoid multiple button presses
+    elif not button.is_pressed and has_printed:
+        has_printed = False
+    sleep(0.01) #avoid CPU dying
