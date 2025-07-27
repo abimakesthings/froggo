be500bbba623057694b6397236e993b2357163f1
abimakesthings <abitriesthings@gmail.com>
2025-07-26 21:57:23 -0700

    file restructuring

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-import RPi.GPIO as GPIO
+from gpiozero import Button
+from picamera import PiCamera
+from datetime import datetime
+from signal import pause
 
-BUTTON_PIN = 16
+button = Button(16)
+camera = PiCamera()
+
+while True:
+    if button.is_pressed:
+        print("Button is pressed")
+    else:
+        print("Button is not pressed")
+
+def capture():
+    camera.capture(f'/home/pi/{datetime.now():%Y-%m-%d-%H-%M-%S}.jpg')
+
+button.when_pressed = capture
+
+pause()
